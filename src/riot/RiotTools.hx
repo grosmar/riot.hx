99654ae70b730096036f9036ad825eab30eef269
Francesco Agati <fagati@veespo.com>
2015-05-21 20:47:26 +0200

    wip

--- src/riot/RiotTools.hx
+++ src/riot/RiotTools.hx
@@ -2,7 +2,7 @@ package riot;
 import js.html.Element;
 
 class RiotTools {
-
+  
   public static inline function riot_mount(element:Element,?tagName:String = '*',?opts:Dynamic = null):Array<Dynamic>{
     return untyped riot.Riot.mount2(element,tagName,opts);
   }
