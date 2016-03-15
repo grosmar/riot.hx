5303337feb8e226b89acb6d8542a1fa10c068a6a
Balazs Zubak <balazs.zubak@doclerholding.com>
2016-03-15 18:46:01 +0100

    added root element as public Tag property

--- src/riot/externs/Tag.hx
+++ src/riot/externs/Tag.hx
@@ -1,10 +1,12 @@
 package riot.externs;
+import js.html.Element;
 
 @:native('riot.Tag')
 extern class Tag {
   
 	var isMounted:Bool;
 	var parent:Tag;
+	var root:Element;
 	var tags:Map<String,Dynamic>;
 	
 	function new(?impl:Impl, ?conf:Conf, ?innerHTML:String);
